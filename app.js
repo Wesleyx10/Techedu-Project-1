81dc338a001a7d295483d4ed83f397a53d6b07b8
Wesley <wesleyorobosa73@gmail.com>
2025-03-29 23:26:14 +0100

    bar adjustment

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+const links = document.querySelector(".links");
+const btn = document.querySelector(".download");
+const opens = document.querySelector(".open");
+const closes = document.querySelector(".close");
 
+opens.addEventListener("click", () => {
+  links.style.display = "flex";
+  btn.style.display = "block";
+  opens.style.display = "none";
+  closes.style.display = "block";
+});
+
+closes.addEventListener("click", () => {
+  links.style.display = "none";
+  btn.style.display = "none";
+  opens.style.display = "block";
+  closes.style.display = "none";
+});
