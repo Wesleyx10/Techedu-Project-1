c2596bf9fe6e9c63082052f7818e8125772f0b91
Wesley <wesleyorobosa73@gmail.com>
2025-03-29 16:38:10 +0100

    remake

--- app.js
+++ app.js
@@ -1,17 +1,1 @@
-const links = document.querySelector(".links");
-const open = document.querySelector(".open");
-const close = document.querySelector(".close");
-const button = document.querySelector(".btns");
 
-open.addEventListener("click", () => {
-  links.style.display = "flex";
-  close.style.display = "block";
-  open.style.display = "none";
-  button.style.display = "block";
-});
-close.addEventListener("click", () => {
-  links.style.display = "none";
-  close.style.display = "none";
-  open.style.display = "block";
-  button.style.display = "none";
-});
